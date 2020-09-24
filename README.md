a003ddff0582797fc230fa8a6ab99e5dac6c328b
Devin Buhl <devin.buhl@pwc.com>
2020-09-24 15:05:51 -0400

    no envars, minor adjustments

--- README.md
+++ README.md
@@ -7,41 +7,15 @@ CLI tool to sign into AWS (MFA) accounts using 1Password
 After following the guide below you can finally use opaws
 
 ```bash
+# First signin to Password CLI if you haven't already
+op signin <domain>.1password.com <op-email-address>
 # Copy script to /usr/local/bin
-cp opaws.sh /usr/local/bin/opaws
+ln -s opaws.sh /usr/local/bin/opaws
 # Run opaws and then provide your 1Password Password at the prompt
-opaws <1Password AWS account title> <domain>.1password.com <aws email address>
+opaws <domain>.1password.com <1password-aws-accounttitle>
 ```
 
 ## Setup Guide
-
-### 1. Install CLI Tools
-
-- Install [1Password CLI](https://app-updates.agilebits.com/product_history/CLI)
-- Install [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html)
-- Install [jq](https://stedolan.github.io/jq/download/)
-
-### 2. Set up Global Environment Variables
-
-Place the follow code in your `~/.bashrc` file then source it or restart your terminal
-
-```bash
-# 1Password Domain
-export OP_DOMAIN="<domain>.1password.com"
-# 1Password Titles for AWS accounts
-export OP_AWS_ACCOUNTS=("AWS-Development" "AWS-Production" "AWS-Explorer")
-# Your AWS Email address
-export AWS_EMAIL=""
-```
-
-### 3. Sign into 1Password on the CLI
-
-```bash
-# Sign into 1Password on the CLI and provide 1Password Secret Key
-op signin ${OP_DOMAIN} ${AWS_EMAIL}
-```
-
-### 4. 1Password & AWS Account Configuration
 
 Make sure each of your AWS entries in 1Password follow this structure
 
--- opaws.sh
+++ opaws.sh
@@ -4,74 +4,93 @@
 # opaws <1Password AWS account title> <domain>.1password.com <aws email address>
 #
 
-AWS_ACCOUNT="${1}"
-OP_DOMAIN="${2}"
-AWS_EMAIL="${3}"
+OP_DOMAIN="${1}"
+OP_AWS_ACCOUNT="${2}"
+LOG_LEVEL=${3:-1}
+
 
 # Exit script if we don't have the necessary environment variables
 if
-  [[ -z "${AWS_ACCOUNT}" ]] ||
-  [[ -z "${OP_DOMAIN}" ]] ||
-  [[ -z "${AWS_EMAIL}" ]];
+  [[ -z "${OP_AWS_ACCOUNT}" ]] ||
+  [[ -z "${OP_DOMAIN}" ]];
 then
-  echo "Error: Missing variables, see README.md"
+  echo "Missing arguments. Aborting."
   exit 1
 fi
 
 # Verify all the command line tools are installed
-need() {
-  if ! [ -x "$(command -v $1)" ]; then
-    echo "Error: $1 is not installed, see README.md"
+command -v op >/dev/null 2>&1 || {
+    echo >&2 "op is not installed. Aborting."
     exit 1
-  fi
+}
+command -v aws >/dev/null 2>&1 || {
+    echo >&2 "aws is not installed. Aborting."
+    exit 1
+}
+command -v jq >/dev/null 2>&1 || {
+    echo >&2 "jq is not installed. Aborting."
+    exit 1
 }
 
-need "jq"
-need "op"
-need "aws"
-
-echo "Signing into 1Password"
+echo "Signing into 1Password..."
 
 # Sign into 1Password
 eval $(op signin "${OP_DOMAIN}")
 
+echo "Signing into aws..."
+
 # Retrieve the item from 1Password
-op_item_json=$(op get item ${AWS_ACCOUNT})
+op_item_json=$(op get item ${OP_AWS_ACCOUNT})
 
-# Extract AWS account ID from Website URL
-account_url=$(op get item AWS-Explorer | jq -r '.overview.url')
-echo "DEBUG - Account URL: ${account_url}"
+# Extract AWS account information from 1Password
+account_email=$(op get item ${OP_AWS_ACCOUNT} | jq -r '.overview.ainfo')
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - Account Email: ${account_email}"
+account_url=$(op get item ${OP_AWS_ACCOUNT} | jq -r '.overview.url')
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - Account URL: ${account_url}"
 account_id=$(echo $account_url | awk -F[/:] '{print $4}' | awk -F[.] '{print $1}')
-echo "DEBUG - Account ID: ${account_id}"
-
-# Extract Section labels and values from 1Password for access and secret keys
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - Account ID: ${account_id}"
 account_access_key=$(echo ${op_item_json} | jq -r '.details.sections[] | select(.title=="Keys") .fields[] | select(.t=="access") | .v')
-echo "DEBUG - Account Access Key: ${account_access_key}"
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - Account Access Key: ${account_access_key}"
 account_secret_key=$(echo ${op_item_json} | jq -r '.details.sections[] | select(.title=="Keys") .fields[] | select(.t=="secret") | .v')
-echo "DEBUG - Account Secret Key: ${account_secret_key}"
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - Account Secret Key: ${account_secret_key}"
 
 # Set the aws profiles keys
-aws --profile "${AWS_ACCOUNT}-default" configure set aws_access_key_id "$account_access_key"
-aws --profile "${AWS_ACCOUNT}-default" configure set aws_secret_access_key "$account_secret_key"
+aws --profile "${OP_AWS_ACCOUNT}-default" configure set aws_access_key_id "${account_access_key}"
+aws --profile "${OP_AWS_ACCOUNT}-default" configure set aws_secret_access_key "${account_secret_key}"
 
-aws_arn="arn:aws:iam::${account_id}:mfa/${AWS_EMAIL}"
-echo "DEBUG - AWS ARN: ${aws_arn}"
+# Set the full arn url
+aws_arn="arn:aws:iam::${account_id}:mfa/${account_email}"
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - AWS ARN: ${aws_arn}"
 
-read aws_access_key_id aws_secret_access_key aws_session_token <<< $( aws \
-  --profile "${AWS_ACCOUNT}-default" sts get-session-token \
-  --duration "129600" \
+# Get the MFA code
+token=$(op get totp ${OP_AWS_ACCOUNT})
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - MFA Token: ${token}"
+
+# Sign in and get the AWS Credential information
+creds=$(aws \
+  --profile "${OP_AWS_ACCOUNT}-default" sts get-session-token \
+  --duration "86400" \
   --serial-number "${aws_arn}" \
-  --token-code $(op get totp ${AWS_ACCOUNT} || exit 5) \
-  --output text  | awk '{ print $2, $4, $5 }')
+  --token-code "${token}" \
+  --output json
+)
+
+# Set the AWS account information
+aws_access_key_id=$(echo ${creds} | jq -r '.Credentials.AccessKeyId')
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - Temporary access key id: ${aws_access_key_id}"
+aws_secret_access_key=$(echo ${creds} | jq -r '.Credentials.SecretAccessKey')
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - Temporary secret access key: ${aws_secret_access_key}"
+aws_session_token=$(echo ${creds} | jq -r '.Credentials.SessionToken')
+[[ ${LOG_LEVEL} -ge 2 ]] && echo "DEBUG - Temporary session token: ${aws_session_token}"
 
 # Set the aws profile session tokens
-aws --profile "default" configure set aws_access_key_id "$aws_access_key_id"
-aws --profile "default" configure set aws_secret_access_key "$aws_secret_access_key"
-aws --profile "default" configure set aws_session_token "$aws_session_token"
+aws --profile "default" configure set aws_access_key_id "${aws_access_key_id}"
+aws --profile "default" configure set aws_secret_access_key "${aws_secret_access_key}"
+aws --profile "default" configure set aws_session_token "${aws_session_token}"
 
 # Verify success or failure
-if aws sts get-caller-identity; then
-  echo "Successfully logged in"
+if aws sts get-caller-identity >/dev/null 2>&1; then
+  echo "aws cli is ready to roll"
 else
-  echo "Error unable to log in"
+  echo "Unable to log in, try to run again with a third parameter set to 2"
 fi
